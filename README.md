7d89dec9c040569f4f1bba83fe9f936e1f7b04ce
Deo Prasad Shaw <deoshaw3@gmail.com>
2023-05-28 15:11:29 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SeleniumAutomationWithCSharp
+This Repository contains project for Selenium with C#
---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+namespace SeleniumAutomationWithCSharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+            TestContext.Progress.WriteLine("I am in Setup");
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            TestContext.Progress.WriteLine("I am in Test1");
+            //Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+
+            TestContext.Progress.WriteLine("I am in Test2");
+            //Assert.Pass();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+
+            TestContext.Progress.WriteLine("I am in TearDown");
+            //Assert.Pass();
+        }
+    }
+}
