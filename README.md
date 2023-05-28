47db02fd399db8672039983eba958ee68894aabf
Deo Prasad Shaw <deoshaw3@gmail.com>
2023-05-28 17:21:30 +0530

    Added Selenium Test

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ This Repository contains project for Selenium with C#
 
 In Order to open this project in visual studio after cloning
 1. Create a solution and project in visual studio
+2. Right click on solution to add the C# project file
+3. This should import the project
+
+If above does not work
+Then,
+
+1. Create a new solution and project in visual studio
 2. Copy all the files from cloned project in the newly created project
---
+++ SeleniumTest1.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomationWithCSharp
+{
+    public class SeleniumTest1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.FullScreen();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+            
+
+            TestContext.Progress.WriteLine("Page title is: "+driver.Title);
+
+            TestContext.Progress.WriteLine("Page title is: " + driver.Url);
+
+            driver.FindElement(By.XPath("//input[@name='username']")).SendKeys("Deo");
+            driver.FindElement(By.XPath("//input[@name='password']")).SendKeys("Deo");
+            driver.FindElement(By.Id("terms")).Click();
+            driver.FindElement(By.Id("signInBtn")).Click();
+            WebDriverWait wait=new WebDriverWait(driver,TimeSpan.FromSeconds(5));
+            Thread.Sleep(2000);
+
+           IWebElement errorElement= driver.FindElement(By.CssSelector(".alert.alert-danger.col-md-12"));
+            String errorMsg = errorElement.Text;
+            TestContext.Progress.Write("Error message is: "+errorMsg);
+
+             Assert.AreEqual("Incorrect username/password.",errorMsg);
+
+           
+
+
+
+
+
+
+        }
+        [TearDown]
+        public void closeBrowser()
+        {
+
+            driver.Quit();
+
+        }
+    }
+}
