62a71e61b1f459822f0107fa3eb84cbd5757f1fa
Deo Prasad Shaw <deoshaw3@gmail.com>
2023-06-27 09:05:04 +0530

    Added US Open test

---
+++ PageObjects/USOpen.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+
+
+namespace SeleniumAutomationWithCSharp.PageObjects
+{
+    internal class USOpen
+    {
+        IWebDriver driver;
+
+
+        public USOpen(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+
+
+        }
+
+        By championImage = By.XPath("//img[contains(@alt,'LA Confidential')]");
+
+
+        internal void takeChampionImage()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            IWebElement championPic=wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(championImage));
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            Screenshot ss= ts.GetScreenshot();
+            //TestContext.Progress.WriteLine(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar);
+            ss.SaveAsFile("C:\\Selenium Projects\\SeleniumAutomationWithCSharp" + Path.DirectorySeparatorChar + "Champion.png");
+
+        }
+    }
+}
