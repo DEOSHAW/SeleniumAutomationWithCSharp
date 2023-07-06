852985ecc6f15f511fd4fadea1b2c9a70417840c
Deo Prasad Shaw <deoshaw3@gmail.com>
2023-07-06 17:22:14 +0530

    Added Keyboard actions test

---
+++ PageObjects/RSPractice2.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomationWithCSharp.PageObjects
+{
+    internal class RSPractice2
+    {
+        IWebDriver driver;
+
+
+        public RSPractice2(IWebDriver driver) 
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        
+        }
+        [FindsBy(How = How.XPath, Using = "//input[@id='name']")]
+        IWebElement nameTextBox;
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='displayed-text']")]
+        IWebElement displayedTextBox;
+
+        internal void copyPasteText()
+        {
+
+            nameTextBox.SendKeys("Deo Shaw");
+            Actions actions = new Actions(driver);
+            actions.KeyDown(Keys.Control).SendKeys("a").KeyUp(Keys.Control).Perform();
+            actions.KeyDown(Keys.Control).SendKeys("c").KeyUp(Keys.Control).Perform();
+            displayedTextBox.Click();
+            actions.KeyDown(Keys.Control).SendKeys("v").KeyUp(Keys.Control).Perform();
+
+            Thread.Sleep(1000);
+
+
+        }
+    }
+}
---
+++ Tests/RSPractice2Test.cs
@@ -0,0 +1,25 @@
+﻿using SeleniumAutomationWithCSharp.Base;
+using SeleniumAutomationWithCSharp.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomationWithCSharp.Tests
+{
+    internal class RSPractice2Test:BaseClass
+    {
+
+        [Test]
+        public void testKeyboardActions()
+        {
+
+            driver.Url = "https://rahulshettyacademy.com/AutomationPractice/";
+            RSPractice2 ob = new RSPractice2(driver);
+            ob.copyPasteText();
+
+
+        }
+    }
+}
